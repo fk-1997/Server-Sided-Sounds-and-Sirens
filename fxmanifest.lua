0b3345a4bfc45df0637d5bd9963453364ce33a13
fk-1997 <70662842+fk-1997@users.noreply.github.com>
2021-11-09 23:21:35 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,48 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'OISS'
+description 'Server Sided Audio'
+version '4.8.6'
+
+files { 
+	"dlc_serversideaudio/oiss_ssa_vehaud_lspd_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lspd_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lssd_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lssd_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_bcso_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_bcso_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_sahp_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_sahp_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_sahp_bike.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_noose_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_noose_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_fib_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_fib_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_rhpd_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_rhpd_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_dppd_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_dppd_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lsia_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lsia_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lspp_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lspp_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lsfd_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lsfd_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lscofd_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_lscofd_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_bcfd_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_bcfd_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_sanfire_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_sanfire_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_sams_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_sams_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_usfs_new.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_usfs_old.awc",
+	"dlc_serversideaudio/oiss_ssa_vehaud_etc.awc",
+	"data/serversideaudio_sounds.dat54.nametable",
+	"data/serversideaudio_sounds.dat54.rel"
+}
+
+data_file "AUDIO_WAVEPACK" "dlc_serversideaudio"
+data_file "AUDIO_SOUNDDATA" "data/serversideaudio_sounds.dat"
